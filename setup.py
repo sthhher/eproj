f96318371ad75da461add389d3dced76723cde4d
sthhhher <esvemira@hotmail.com>
2020-02-04 10:45:11 +0100

    pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
-from distutils.core import setup
-setup(
-    name = 'eprojj',
-    packages = ['eprojj'], # this must be the same as the name above
-    version = '0.1',
-    description = 'Pass the file of github to pipy',
-    author = 'Esther Vendrell',
-    author_email = 'esvemira@hotmail.com',
-    url = 'https://github.com/sthhher/eproj.git', # use the URL to the github repo
-    download_url = 'https://github.com/sthhher/eproj.git/tarball/0.1',
-    keywords = ['testing', 'logging', 'example'],
-    classifiers = [],
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="eproj",
+    version="0.0.1",
+    author="Esther Vendrell",
+    author_email="esvemira@hotmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sthhher/eproj",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
